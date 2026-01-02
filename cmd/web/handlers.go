f46894b1579bff0a69ad8e364a0f250a71147a44
Max Kranker <maxkranker@protonmail.com>
2026-01-02 14:18:27 +0700

    Add a navigation template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -20,6 +20,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
 		"./ui/html/base.tmpl",
 		"./ui/html/pages/home.tmpl",
+		"./ui/html/partials/nav.tmpl",
 	}
 
 	// parse the home page template files
