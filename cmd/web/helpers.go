515c994cca40ebd2ad8933b822dfade2dc6a907c
Max Kranker <maxkranker@protonmail.com>
2026-01-03 19:25:29 +0700

    Add helper error functions

---
+++ cmd/web/helpers.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+// serverError writes an error message and the
+// stack trace to the errorLog. then sends a
+// generic 500 internal server error response
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Println(trace)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// clientError sends a specific status code
+// and corresponding description to the user
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+// notFound uses the clientError function to
+// send a notFound 404 response
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
