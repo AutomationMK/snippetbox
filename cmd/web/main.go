b5fcb4b8dc45f1ab5eeae22dcdf411809b3e9647
Max Kranker <maxkranker@protonmail.com>
2026-01-03 18:51:50 +0700

    Add info and error logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -11,6 +12,13 @@ func main() {
 	// default value of :4000
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
+
+	// create logger for info messages
+	// has extra flags for any more info to add
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// create logger for error messages
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
@@ -21,9 +29,9 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Printf("Starting server on %s", *addr)
+	infoLog.Printf("Starting server on %s", *addr)
 	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
-		log.Fatal(err)
+		errorLog.Fatal(err)
 	}
 }
