bc88d80b120489f506c7d57e6e2d51130aa0a77b
Max Kranker <maxkranker@protonmail.com>
2026-01-04 21:22:51 +0700

    Add snippets template data for home page

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -5,5 +5,6 @@ import "github.com/AutomationMK/snippetbox/internal/models"
 // define template data struct to hold dynamic data
 // for html templates
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
+	Snippets []*models.Snippet
 }
