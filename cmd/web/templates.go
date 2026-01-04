a1e8847fe75d978bae9187b4f6cd7ee3de5f4acb
Max Kranker <maxkranker@protonmail.com>
2026-01-04 20:53:38 +0700

    Add templateData struct to wrap dynamic template data

---
+++ cmd/web/templates.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/AutomationMK/snippetbox/internal/models"
+
+// define template data struct to hold dynamic data
+// for html templates
+type templateData struct {
+	Snippet *models.Snippet
+}
