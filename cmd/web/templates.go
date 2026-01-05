e6eda4f22aba32b25a466fffec6373d245d14aa0
Max Kranker <maxkranker@protonmail.com>
2026-01-05 17:48:29 +0700

    Add a template cache creation function

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,6 +1,49 @@
 package main
 
-import "github.com/AutomationMK/snippetbox/internal/models"
+import (
+	"path/filepath"
+	"text/template"
+
+	"github.com/AutomationMK/snippetbox/internal/models"
+)
+
+// newTemplateCache creates a template cache
+func newTemplateCache() (map[string]*template.Template, error) {
+	// initialize a new map to act as the cache
+	cache := map[string]*template.Template{}
+
+	// get a slice of all filepaths
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	// loop through the page filepaths
+	for _, page := range pages {
+		// extract the file name as a variable
+		name := filepath.Base(page)
+
+		// create a slice containing the base template
+		// and any patials along with the page
+		files := []string{
+			"./ui/html/base.tmpl",
+			"./ui/html/partials/nav.tmpl",
+			page,
+		}
+
+		// parse the files into a template set
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			return nil, err
+		}
+
+		// add template set to the map with page name
+		// as the key
+		cache[name] = ts
+	}
+
+	return cache, nil
+}
 
 // define template data struct to hold dynamic data
 // for html templates
